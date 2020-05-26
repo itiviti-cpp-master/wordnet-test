b0455e359d926f969a5cb7f92b8c14e144cc0d7f
Evgeniy Osipov <osipov2005@yandex.ru>
2020-05-26 12:06:48 +0300

    add some tests for outcast

--- src/test.cpp
+++ src/test.cpp
@@ -5,18 +5,25 @@ class WordNetTest : public ::testing::Test
 {
 protected:
     static void SetUpTestSuite()
-    { m_wordnet = new WordNet("etc/synsets.txt", "etc/hypernyms.txt"); }
+    {
+        m_wordnet = new WordNet("etc/synsets.txt", "etc/hypernyms.txt"); 
+        m_outcast = new Outcast(*m_wordnet);
+    }
 
     static void TearDownTestSuite()
     {
         delete m_wordnet;
         m_wordnet = nullptr;
+        delete m_outcast;
+        m_outcast = nullptr;
     }
 
     static WordNet * m_wordnet;
+    static Outcast * m_outcast;
 };
 
 WordNet * WordNetTest::m_wordnet = nullptr;
+Outcast * WordNetTest::m_outcast = nullptr;
 
 TEST_F(WordNetTest, Basic)
 {
@@ -53,5 +60,43 @@ TEST_F(WordNetTest, Search)
     ASSERT_TRUE(m_wordnet->distance("Alpena", "Aarhus") == 2);
     ASSERT_TRUE(m_wordnet->is_noun("position"));
     ASSERT_TRUE(m_wordnet->distance("Aarhus", "position") == 4);
+}
 
+TEST_F(WordNetTest, Outcast)
+{
+    std::vector<std::string> d1 {"Turing"};
+    ASSERT_TRUE(m_outcast->outcast(d1) == "");
+
+    std::vector<std::string> d2 {"Turing", "von_Neumann"};
+    ASSERT_TRUE(m_outcast->outcast(d2) == "");
+
+    std::vector<std::string> d3 {"Turing", "von_Neumann", "Mickey_Mouse"};
+    ASSERT_TRUE(m_outcast->outcast(d3) == "Mickey_Mouse");
+
+    std::vector<std::string> d5 {"horse", "zebra", "cat", "bear", "table"};
+    ASSERT_TRUE(m_outcast->outcast(d5) == "table");
+
+    std::vector<std::string> d6 {"earth", "fire", "air", "water", "heart"};
+    ASSERT_TRUE(m_outcast->outcast(d6) == "heart");
+
+    std::vector<std::string> d7 {"Asia", "Australia", "North_America", "India", "Europe", "Antarctica", "South_America"};
+    ASSERT_TRUE(m_outcast->outcast(d7) == "India");
+
+    std::vector<std::string> d8 {"water", "soda", "bed", "orange_juice", "milk", "apple_juice", "tea", "coffee"};
+    ASSERT_TRUE(m_outcast->outcast(d8) == "bed");
+
+    std::vector<std::string> d9 {"Banti's_disease", "hyperadrenalism", "German_measles", "gargoylism", "Q_fever", "amebiosis", "anthrax", "playboy"};
+    ASSERT_TRUE(m_outcast->outcast(d9) == "playboy");
+
+    std::vector<std::string> d10 {"apple", "orange", "banana", "grape", "strawberry", "cabbage", "mango", "watermelon"};
+    ASSERT_TRUE(m_outcast->outcast(d10) == "cabbage");
+
+    std::vector<std::string> d14 {"cat", "cheetah", "dog", "wolf", "albatross", "horse", "zebra", "lemur", "orangutan", "chimpanzee"};
+    ASSERT_TRUE(m_outcast->outcast(d14) == "albatross");
+
+    std::vector<std::string> d15 {"blue", "green", "yellow", "brown", "black", "white", "orange", "violet", "red", "serendipity"};
+    ASSERT_TRUE(m_outcast->outcast(d15) == "serendipity");
+
+    std::vector<std::string> d16 {"apple", "pear", "peach", "banana", "lime", "lemon", "blueberry", "strawberry", "mango", "watermelon", "potato"};
+    ASSERT_TRUE(m_outcast->outcast(d16) == "potato");
 }
