5426d4a2017bd1c747286cd1c715500781bcc681
Dmitri Shubin <Dmitri.Shubin@itiviti.com>
2022-04-11 13:02:43 +0300

    rework and update tests

--- src/test.cpp
+++ src/test.cpp
@@ -1,141 +1,162 @@
-#include <gtest/gtest.h>
 #include "wordnet.h"
 #include "test_iterator.h"
+
+#include <gtest/gtest.h>
+
+#include <algorithm>
+#include <fstream>
+#include <memory>
+#include <sstream>
+#include <vector>
+
+namespace {
 
 class WordNetTest : public ::testing::Test
 {
 protected:
     static void SetUpTestSuite()
     {
-        m_wordnet = new WordNet("tests/etc/synsets.txt", "tests/etc/hypernyms.txt"); 
-        m_outcast = new Outcast(*m_wordnet);
+        std::ifstream synsets{"tests/etc/synsets.txt"}, hypernyms{"tests/etc/hypernyms.txt"};
+        ASSERT_TRUE(synsets);
+        ASSERT_TRUE(hypernyms);
+
+        m_wordnet = std::make_unique<WordNet>(synsets, hypernyms);
+        m_outcast = std::make_unique<Outcast>(*m_wordnet);
     }
 
     static void TearDownTestSuite()
     {
-        delete m_wordnet;
-        m_wordnet = nullptr;
-        delete m_outcast;
-        m_outcast = nullptr;
+        m_wordnet.reset();
+        m_outcast.reset();
     }
 
-    WordNet & not_empty_container()
+    WordNet::Nouns not_empty_container()
     {
-        return *m_wordnet;
+        return m_wordnet->nouns();
     }
 
-    using iterator_t = WordNet::iterator;
-    static WordNet * m_wordnet;
-    static Outcast * m_outcast;
+    using iterator_t = WordNet::Nouns::iterator;
+
+    static std::unique_ptr<WordNet> m_wordnet;
+    static std::unique_ptr<Outcast> m_outcast;
 };
 
-WordNet * WordNetTest::m_wordnet = nullptr;
-Outcast * WordNetTest::m_outcast = nullptr;
+std::unique_ptr<WordNet> WordNetTest::m_wordnet;
+std::unique_ptr<Outcast> WordNetTest::m_outcast;
+
+//------------------------------------------------------------------------------
+
+TEST_F(WordNetTest, nouns)
+{
+    std::istringstream synsets{R"(
+82145,zombi zombie living_dead,a dead body that has been brought back to life by a supernatural force
+82146,zombi zombie snake_god,a god of voodoo cults of African origin worshipped especially in West Indies  
+)"};
+    
+    std::istringstream hypernyms;
+
+    WordNet wordnet{synsets, hypernyms};
+    EXPECT_TRUE(wordnet.is_noun("zombi"));
+    EXPECT_TRUE(wordnet.is_noun("zombie"));
+    EXPECT_TRUE(wordnet.is_noun("living_dead"));
+    EXPECT_FALSE(wordnet.is_noun("zomb"));
+
+    std::vector<std::string> nouns{wordnet.nouns().begin(), wordnet.nouns().end()};
+    std::sort(nouns.begin(), nouns.end());
+
+    EXPECT_EQ(nouns, (std::vector<std::string>{"living_dead", "snake_god", "zombi", "zombie"}));
+}
+
+//------------------------------------------------------------------------------
+
+TEST_F(WordNetTest, distance)
+{
+    std::istringstream synsets{R"(
+1,a a1,a gloss
+2,b bd,b gloss
+3,c,c gloss
+4,d bd,d gloss
+)"};
+    
+    std::istringstream hypernyms{R"(
+2,1
+3,1
+4,2,3
+)"};
+
+    WordNet wordnet{synsets, hypernyms};
+
+    EXPECT_EQ(wordnet.distance("a", "a"), 0);
+    EXPECT_EQ(wordnet.distance("a", "a1"), 0);
+    EXPECT_EQ(wordnet.distance("a", "b"), 1);
+    EXPECT_EQ(wordnet.distance("b", "a"), 1);
+    EXPECT_EQ(wordnet.distance("b", "c"), 2);
+
+    EXPECT_EQ(wordnet.distance("d", "a"), 2);
+    EXPECT_EQ(wordnet.distance("d", "c"), 1);
+
+    EXPECT_EQ(wordnet.distance("bd", "c"), 1);
+}
+
+//------------------------------------------------------------------------------
 
 TEST_F(WordNetTest, Basic)
 {
-    ASSERT_TRUE(m_wordnet->is_noun("whole-word_method"));
-    ASSERT_TRUE(m_wordnet->is_noun("genus_Commiphora"));
-    ASSERT_TRUE(m_wordnet->is_noun("barbershop_quartet"));
+    EXPECT_TRUE(m_wordnet->is_noun("whole-word_method"));
+    EXPECT_TRUE(m_wordnet->is_noun("genus_Commiphora"));
+    EXPECT_TRUE(m_wordnet->is_noun("barbershop_quartet"));
 
-    auto it = m_wordnet->begin();
-    auto end = m_wordnet->end();
-    ASSERT_TRUE(it != end);
-    size_t n = 0;
-    while (it != end) {
-        ++n;
-        ++it;
-    }
-
-    ASSERT_TRUE(119188 == n);
+    EXPECT_EQ(std::distance(m_wordnet->nouns().begin(), m_wordnet->nouns().end()), 119188);
 }
 
 TEST_F(WordNetTest, Search)
 {
-    ASSERT_TRUE(m_wordnet->is_noun("Amazon"));
-    ASSERT_TRUE(m_wordnet->distance("Amazon", "Amazon") == 0);
-    ASSERT_TRUE(m_wordnet->is_noun("application-oriented_language"));
-    ASSERT_TRUE(m_wordnet->is_noun("problem-oriented_language"));
-    ASSERT_TRUE(m_wordnet->distance("application-oriented_language", "problem-oriented_language") == 0);
-    ASSERT_TRUE(m_wordnet->is_noun("Alopius"));
-    ASSERT_TRUE(m_wordnet->is_noun("Alosa"));
-    ASSERT_TRUE(m_wordnet->distance("Alopius", "Alosa") == 2);
-    ASSERT_TRUE(m_wordnet->is_noun("Alpena"));
-    ASSERT_TRUE(m_wordnet->is_noun("Appleton"));
-    ASSERT_TRUE(m_wordnet->distance("Alpena", "Appleton") == 2);
-    ASSERT_TRUE(m_wordnet->is_noun("Aarhus"));
-    ASSERT_TRUE(m_wordnet->distance("Alpena", "Aarhus") == 2);
-    ASSERT_TRUE(m_wordnet->is_noun("position"));
-    ASSERT_TRUE(m_wordnet->distance("Aarhus", "position") == 4);
+    EXPECT_TRUE(m_wordnet->is_noun("Amazon"));
+    EXPECT_EQ(m_wordnet->distance("Amazon", "Amazon"), 0);
+    EXPECT_TRUE(m_wordnet->is_noun("application-oriented_language"));
+    EXPECT_TRUE(m_wordnet->is_noun("problem-oriented_language"));
+    EXPECT_EQ(m_wordnet->distance("application-oriented_language", "problem-oriented_language"), 0);
+    EXPECT_TRUE(m_wordnet->is_noun("Alopius"));
+    EXPECT_TRUE(m_wordnet->is_noun("Alosa"));
+    EXPECT_EQ(m_wordnet->distance("Alopius", "Alosa"), 2);
+    EXPECT_TRUE(m_wordnet->is_noun("Alpena"));
+    EXPECT_TRUE(m_wordnet->is_noun("Appleton"));
+    EXPECT_EQ(m_wordnet->distance("Alpena", "Appleton"), 2);
+    EXPECT_TRUE(m_wordnet->is_noun("Aarhus"));
+    EXPECT_EQ(m_wordnet->distance("Alpena", "Aarhus"), 2);
+    EXPECT_TRUE(m_wordnet->is_noun("position"));
+    EXPECT_EQ(m_wordnet->distance("Aarhus", "position"), 4);
+    EXPECT_EQ(m_wordnet->distance("nose", "ear"), 3);
+    EXPECT_EQ(m_wordnet->distance("ear", "nose"), 3);
+    EXPECT_EQ(m_wordnet->distance("ware", "tinware"), 2);
+    EXPECT_EQ(m_wordnet->distance("gesso", "defoliant"), 5);
 }
 
 TEST_F(WordNetTest, Outcast)
 {
-    std::vector<std::string> d1 {"Turing"};
-    ASSERT_TRUE(m_outcast->outcast(d1).empty());
-
-    std::vector<std::string> d2 {"Turing", "von_Neumann"};
-    ASSERT_TRUE(m_outcast->outcast(d2).empty());
-
-    std::vector<std::string> d3 {"Turing", "von_Neumann", "Mickey_Mouse"};
-    ASSERT_TRUE(m_outcast->outcast(d3) == "Mickey_Mouse");
-
-    std::vector<std::string> d5 {"horse", "zebra", "cat", "bear", "table"};
-    ASSERT_TRUE(m_outcast->outcast(d5) == "table");
-
-    std::vector<std::string> d4 {"probability", "statistics", "mathematics", "physics"};
-    ASSERT_TRUE(m_outcast->outcast(d4) == "probability");
-
-    std::vector<std::string> d6 {"earth", "fire", "air", "water", "heart"};
-    ASSERT_TRUE(m_outcast->outcast(d6) == "heart");
-
-    std::vector<std::string> d7 {"Asia", "Australia", "North_America", "India", "Europe", "Antarctica", "South_America"};
-    ASSERT_TRUE(m_outcast->outcast(d7) == "India");
-
-    std::vector<std::string> d8 {"water", "soda", "bed", "orange_juice", "milk", "apple_juice", "tea", "coffee"};
-    ASSERT_TRUE(m_outcast->outcast(d8) == "bed");
-
-    std::vector<std::string> d9 {"Banti's_disease", "hyperadrenalism", "German_measles", "gargoylism", "Q_fever", "amebiosis", "anthrax", "playboy"};
-    ASSERT_TRUE(m_outcast->outcast(d9) == "playboy");
-
-    std::vector<std::string> d10 {"apple", "orange", "banana", "grape", "strawberry", "cabbage", "mango", "watermelon"};
-    ASSERT_TRUE(m_outcast->outcast(d10) == "cabbage");
-
-    std::vector<std::string> d11 {"car", "auto", "truck", "plane", "tree", "train", "vehicle", "van"};
-    ASSERT_TRUE(m_outcast->outcast(d11) == "tree");
-
-    std::vector<std::string> d12 {"lumber", "wood", "tree", "leaf", "nail", "house", "building", "edifice", "structure"};
-    ASSERT_TRUE(m_outcast->outcast(d12) == "tree");
-
-    /*
-    std::vector<std::string> d13 {"hair", "eyes", "arm", "mouth", "nose", "ear", "cheek", "brow", "chin"};
-    ASSERT_TRUE(m_outcast->outcast(d13) == "arm");
-    */
-
-    std::vector<std::string> d14 {"cat", "cheetah", "dog", "wolf", "albatross", "horse", "zebra", "lemur", "orangutan", "chimpanzee"};
-    ASSERT_TRUE(m_outcast->outcast(d14) == "albatross");
-
-    std::vector<std::string> d15 {"blue", "green", "yellow", "brown", "black", "white", "orange", "violet", "red", "serendipity"};
-    ASSERT_TRUE(m_outcast->outcast(d15) == "serendipity");
-
-    std::vector<std::string> d16 {"apple", "pear", "peach", "banana", "lime", "lemon", "blueberry", "strawberry", "mango", "watermelon", "potato"};
-    ASSERT_TRUE(m_outcast->outcast(d16) == "potato");
-
-    // Extra test coverage by @dedlocc
-    ASSERT_TRUE(m_wordnet->distance("nose", "ear") == 3);
-    ASSERT_TRUE(m_wordnet->distance("ear", "nose") == 3);
-    ASSERT_TRUE(m_wordnet->distance("ware", "tinware") == 2);
-    ASSERT_TRUE(m_wordnet->distance("gesso", "defoliant") == 5);
-
-    std::vector<std::string> d17 {"oak", "pine", "birch", "acacia"};
-    ASSERT_TRUE(m_outcast->outcast(d17).empty());
-    //
+    EXPECT_EQ(m_outcast->outcast({"Turing"}), "");
+    EXPECT_EQ(m_outcast->outcast({"Turing", "von_Neumann"}), "");
+    EXPECT_EQ(m_outcast->outcast({"Turing", "von_Neumann", "Mickey_Mouse"}), "Mickey_Mouse");
+    EXPECT_EQ(m_outcast->outcast({"horse", "zebra", "cat", "bear", "table"}), "table");
+    EXPECT_EQ(m_outcast->outcast({"probability", "statistics", "mathematics", "physics"}), "probability");
+    EXPECT_EQ(m_outcast->outcast({"earth", "fire", "air", "water", "heart"}), "heart");
+    EXPECT_EQ(m_outcast->outcast({"Asia", "Australia", "North_America", "India", "Europe", "Antarctica", "South_America"}), "India");
+    EXPECT_EQ(m_outcast->outcast({"water", "soda", "bed", "orange_juice", "milk", "apple_juice", "tea", "coffee"}), "bed");
+    EXPECT_EQ(m_outcast->outcast({"Banti's_disease", "hyperadrenalism", "German_measles", "gargoylism", "Q_fever", "amebiosis", "anthrax", "playboy"}), "playboy");
+    EXPECT_EQ(m_outcast->outcast({"apple", "orange", "banana", "grape", "strawberry", "cabbage", "mango", "watermelon"}), "cabbage");
+    EXPECT_EQ(m_outcast->outcast({"car", "auto", "truck", "plane", "tree", "train", "vehicle", "van"}), "tree");
+    EXPECT_EQ(m_outcast->outcast({"lumber", "wood", "tree", "leaf", "nail", "house", "building", "edifice", "structure"}), "tree");
+    //EXPECT_EQ(m_outcast->outcast({"hair", "eyes", "arm", "mouth", "nose", "ear", "cheek", "brow", "chin"}), "arm");
+    EXPECT_EQ(m_outcast->outcast({"cat", "cheetah", "dog", "wolf", "albatross", "horse", "zebra", "lemur", "orangutan", "chimpanzee"}), "albatross");
+    EXPECT_EQ(m_outcast->outcast({"blue", "green", "yellow", "brown", "black", "white", "orange", "violet", "red", "serendipity"}), "serendipity");
+    EXPECT_EQ(m_outcast->outcast({"apple", "pear", "peach", "banana", "lime", "lemon", "blueberry", "strawberry", "mango", "watermelon", "potato"}), "potato");
+    EXPECT_EQ(m_outcast->outcast({"oak", "pine", "birch", "acacia"}), "");
 }
 
 TEST_F(WordNetTest, MultiThreadIteratorAccess)
 {
-    auto it = m_wordnet->begin();
-    auto end = m_wordnet->end();
+    auto it = m_wordnet->nouns().begin();
+    auto end = m_wordnet->nouns().end();
 
     std::vector<iterator_test::Job<iterator_t>> jobs;
     size_t count = 10;
@@ -148,3 +169,5 @@ TEST_F(WordNetTest, MultiThreadIteratorAccess)
 
 using TypesToTest = ::testing::Types<WordNetTest>;
 INSTANTIATE_TYPED_TEST_SUITE_P(WN, IteratorTest, TypesToTest);
+
+} // unnamed namespace
